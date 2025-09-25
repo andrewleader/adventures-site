ec83e491156da793d934b3feda55564ce88f4eac
Andrew Leader <aleader@microsoft.com>
2025-09-25 11:33:25 -0700

    Display "5.10d"

--- app/map/client-page.tsx
+++ app/map/client-page.tsx
@@ -307,7 +307,11 @@ export default function MapClientPage({ areasData, routesData }: MapClientPagePr
                     )}
                     <div className="text-sm text-gray-600 mt-1">
                       {routeData.classRating && (
-                        <span>🧗 {routeData.classRating.replace('class', 'Class ')}</span>
+                        <span>🧗 {
+                          routeData.classRating === 'class5' && routeData.ydsRating
+                            ? `5.${routeData.ydsRating}${routeData.ydsSubRating || ''}`
+                            : routeData.classRating.replace('class', 'Class ')
+                        }</span>
                       )}
                       {routeData.miles && (
                         <span className="ml-2">📏 {routeData.miles}mi</span>
@@ -444,7 +448,10 @@ export default function MapClientPage({ areasData, routesData }: MapClientPagePr
                         </span>
                         {selectedLocation.routes[0].classRating && (
                           <span className="ml-2 text-xs bg-gray-100 text-gray-700 px-2 py-1 rounded">
-                            {selectedLocation.routes[0].classRating.replace('class', 'Class ')}
+                            {selectedLocation.routes[0].classRating === 'class5' && selectedLocation.routes[0].ydsRating
+                              ? `5.${selectedLocation.routes[0].ydsRating}${selectedLocation.routes[0].ydsSubRating || ''}`
+                              : selectedLocation.routes[0].classRating.replace('class', 'Class ')
+                            }
                           </span>
                         )}
                       </div>
@@ -505,7 +512,9 @@ export default function MapClientPage({ areasData, routesData }: MapClientPagePr
                         {selectedLocation.routes && selectedLocation.routes.length > 0 && (
                           <span className="ml-2 text-xs bg-gray-100 text-gray-700 px-2 py-1 rounded">
                             {selectedLocation.type === 'route' 
-                              ? selectedLocation.routes[0].classRating?.replace('class', 'Class ') || 'Unspecified'
+                              ? (selectedLocation.routes[0].classRating === 'class5' && selectedLocation.routes[0].ydsRating
+                                  ? `5.${selectedLocation.routes[0].ydsRating}${selectedLocation.routes[0].ydsSubRating || ''}`
+                                  : selectedLocation.routes[0].classRating?.replace('class', 'Class ') || 'Unspecified')
                               : `Max: ${getHighestDifficulty(selectedLocation.routes).replace('class', 'Class ') || 'Unspecified'}`
                             }
                           </span>
@@ -595,7 +604,11 @@ export default function MapClientPage({ areasData, routesData }: MapClientPagePr
                 {location.type === 'route' && location.routes && location.routes[0] && (
                   <div className="text-sm text-gray-600 mt-1">
                     {location.routes[0].classRating && (
-                      <span>🧗 {location.routes[0].classRating.replace('class', 'Class ')}</span>
+                      <span>🧗 {
+                        location.routes[0].classRating === 'class5' && location.routes[0].ydsRating
+                          ? `5.${location.routes[0].ydsRating}${location.routes[0].ydsSubRating || ''}`
+                          : location.routes[0].classRating.replace('class', 'Class ')
+                      }</span>
                     )}
                     {location.routes[0].miles && (
                       <span className="ml-2">📏 {location.routes[0].miles}mi</span>
@@ -649,7 +662,11 @@ export default function MapClientPage({ areasData, routesData }: MapClientPagePr
                           {routeData.miles && <span>📏 {routeData.miles} mi</span>}
                           {routeData.gain && <span className="ml-3">⬆️ {routeData.gain}ft</span>}
                           {routeData.classRating && (
-                            <span className="ml-3">🧗 {routeData.classRating.replace('class', 'Class ')}</span>
+                            <span className="ml-3">🧗 {
+                              routeData.classRating === 'class5' && routeData.ydsRating
+                                ? `5.${routeData.ydsRating}${routeData.ydsSubRating || ''}`
+                                : routeData.classRating.replace('class', 'Class ')
+                            }</span>
                           )}
                         </div>
                       </div>
